6cdc43a69395195064c04370e0316df4e35cd02d
Konstantin Lyubort <lyubortk@gmail.com>
2020-05-27 16:24:28 +0300

    fix

--- task1/run.sh
+++ task1/run.sh
@@ -2,4 +2,4 @@
 
 IMAGE_NAME="lyubortk-task1"
 docker build -f "Dockerfile" -t "$IMAGE_NAME" ./application/
-docker run -v "$(pwd)/script":/data -i "$IMAGE_NAME"
+docker run -v "$(pwd)/script":/data -it "$IMAGE_NAME"
